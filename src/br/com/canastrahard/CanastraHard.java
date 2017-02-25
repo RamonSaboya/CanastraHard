9bc41ffb2f7413505fc0371cbcffc9eae2b9a423
Melissa Correa <melissacorreafalcao@gmail.com>
2017-02-25 12:23:15 -0300

    Signed-off-by: Melissa Correa <melissacorreafalcao@gmail.com>

--- src/br/com/canastrahard/CanastraHard.java
+++ src/br/com/canastrahard/CanastraHard.java
@@ -3,8 +3,9 @@ package br.com.canastrahard;
 public class CanastraHard {
 
 	public static void main(String[] args) {
-		int mel = 65;
-		System.out.println(mel);
+		Carta carta = new Carta((byte) 10, (byte) 2, false);
+		
+		System.out.println(carta.info());
 	}
 
 }
