f379fc93f273c0390e5e3acd734a62a1f837fe3e
Ramon Saboya <ramons.gomes@hotmail.com>
2017-02-25 12:21:25 -0300

    Signed-off-by: Ramon Saboya <ramons.gomes@hotmail.com>

--- src/br/com/canastrahard/CanastraHard.java
+++ src/br/com/canastrahard/CanastraHard.java
@@ -1,10 +1,28 @@
 package br.com.canastrahard;
 
-public class CanastraHard {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class CanastraHard extends Application {
 
 	public static void main(String[] args) {
-		int mel = 65;
-		System.out.println(mel);
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+
+			Scene scene = new Scene(root, 400, 400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/br/com/canastrahard/Carta.java
+++ src/br/com/canastrahard/Carta.java
@@ -2,4 +2,100 @@ package br.com.canastrahard;
 
 public class Carta {
 
+	private byte numero;
+	private byte naipe;
+	private boolean azul;
+
+	public Carta(byte numero, byte naipe, boolean azul) {
+		this.numero = numero;
+		this.naipe = naipe;
+		this.azul = azul;
+	}
+
+	public byte getNumero() {
+		return numero;
+	}
+
+	public byte getNaipe() {
+		return naipe;
+	}
+
+	public boolean isAzul() {
+		return azul;
+	}
+
+	public String info() {
+		String info = "";
+
+		switch (numero) {
+		case 0:
+			info = "Ás";
+			break;
+		case 1:
+			info = "Dois";
+			break;
+		case 2:
+			info = "Três";
+			break;
+		case 3:
+			info = "Quatro";
+			break;
+		case 4:
+			info = "Cinco";
+			break;
+		case 5:
+			info = "Seis";
+			break;
+		case 6:
+			info = "Sete";
+			break;
+		case 7:
+			info = "Oito";
+			break;
+		case 8:
+			info = "Nove";
+			break;
+		case 9:
+			info = "Dez";
+			break;
+		case 10:
+			info = "Valete";
+			break;
+		case 11:
+			info = "Dama";
+			break;
+		case 12:
+			info = "Rei";
+			break;
+		}
+		
+		info += " de ";
+		
+		switch (naipe) {
+		case 0:
+			info += "Espadas";
+			break;
+		case 1:
+			info += "Copas";
+			break;
+		case 2:
+			info += "Paus";
+			break;
+		case 3:
+			info += "Ouros";
+			break;
+		}
+		
+		info += " ";
+		
+		if (azul) {
+			info += "Azul";
+
+		} else {
+			info += "Vermelho";
+		}
+		
+		return info;
+	}
+
 }
